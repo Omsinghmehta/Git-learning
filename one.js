93552b210b02fa257147069cacb9f313ccc7e08f
Om Singh Mehta <ommehta912@gmail.com>
2025-01-21 00:43:02 +0530

    gitgithub_notes

--- one.js
+++ one.js
@@ -9,3 +9,26 @@ console.log("king 2 comm");
 console.log("latest change");
 // fixing the bugs..
 console.log("bug fixed");
+// git status:give the modified and staging
+// git diff: all changes
+// git add fileName: for one file to comes in staging area
+// git add . : all comes in staging area 
+// git commit -m "msg": for commit 
+// git reset fileName:for removing the file from the staging area 
+// git log: info of all commits
+// git log --oneline: similar but short info
+// git reset --hard "id on which head should point" -->
+// <!-- git revert "id" -- for revert the changes of any node -->
+// <!-- git checkout -b "feature"-- create the new "feature" branch -->
+// <!-- git checkout master-- move to master branch  -->
+// <!-- git merge feature-- merge the master and feature branch while on the master branch -->
+// <!-- git branch -D feature-- delete the feature -->
+// <!-- git rebase master-- rebasing 
+// git rebase continue-- constinuing and manage merge conflict
+// ,can change the msg now as well , press i there after Esc and then :wq , then do 
+// same for next one.
+// -- also then finally merge the master to other -->
+// <!-- git branch -M main-- change the name from master to main -->
+// <!-- git pull origin main-- bring the changes made from github(approved by SDE ,after the merge) to out system  -->
+// <!-- git push origin --delete bugfix-- delete the bugfix branch also from the github -->
+// <!-- git branch -r -- give all the branches -->
